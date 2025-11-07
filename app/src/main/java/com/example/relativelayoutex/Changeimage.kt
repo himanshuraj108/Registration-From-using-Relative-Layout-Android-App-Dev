27abb078f446fe89e420fe4d2554c0380a4fab06
Himanshu <himanshuraj48512@gmail.com>
2025-11-07 13:55:32 +0530

    Amazon clone

--- app/src/main/java/com/example/relativelayoutex/Changeimage.kt
+++ app/src/main/java/com/example/relativelayoutex/Changeimage.kt
@@ -4,21 +4,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Changeimage : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_changeimage)
+
         val btn: Button = findViewById(R.id.btn)
         val image: ImageView = findViewById(R.id.image)
 
+        val images = arrayOf(
+            R.drawable.tomimage,
+            R.drawable.jerry,
+        )
+
+        var currentIndex = 0
+
         btn.setOnClickListener {
-            image.setImageResource(R.drawable.jerry)
+            image.setImageResource(images[currentIndex])
+            currentIndex = (currentIndex + 1) % images.size
         }
     }
 }
