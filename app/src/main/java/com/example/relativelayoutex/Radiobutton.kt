dbdd27b9933e4f574461421b396a29d0a55450f7
Himanshu <himanshuraj48512@gmail.com>
2025-11-12 23:48:41 +0530

    update Radio

--- app/src/main/java/com/example/relativelayoutex/Radiobutton.kt
+++ app/src/main/java/com/example/relativelayoutex/Radiobutton.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,12 +21,14 @@ class Radiobutton : AppCompatActivity() {
         var rGrp: RadioGroup = findViewById(R.id.rGrp)
         var btn: Button = findViewById(R.id.submitradio)
         var reset: Button = findViewById(R.id.resetradio)
+        var showResult: TextView = findViewById(R.id.showResult)
 
         btn.setOnClickListener {
             var selId = rGrp.checkedRadioButtonId
             if(selId != -1){
                 var rBtn: RadioButton = findViewById(selId)
                 Toast.makeText(applicationContext,"Your selected option is ${rBtn.text}", Toast.LENGTH_LONG).show();
+                showResult.text = "Your selected preference is ${rBtn.text}"
             }
             else{
                 Toast.makeText(applicationContext,"Option must be selected for submit", Toast.LENGTH_LONG).show()
@@ -33,8 +36,9 @@ class Radiobutton : AppCompatActivity() {
         }
         reset.setOnClickListener {
             var sellid = rGrp.checkedRadioButtonId
-            if(sellid == 1){
-                sellid == -1
+            if(sellid != 1){
+                rGrp.clearCheck()
+                showResult.text = ""
             }
         }
     }
